571e27ef409b0e7d273bbeb24e3272eb52af1130
Chase <chaseit12@gmail.com>
2018-02-26 20:51:24 +0000

    automatically backed up by learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -3,23 +3,23 @@ CREATE TABLE series(
   title TEXT,
   author_id INTEGER,
   subgenre_id INTEGER
-)
+);
 
 CREATE TABLE subgenres(
   id INTEGER PRIMARY KEY,
   name TEXT
-)
+);
 
 CREATE TABLE authors(
   id INTEGER PRIMARY KEY,
   name TEXT
-)
+);
 
 CREATE TABLE books(
   id INTEGER PRIMARY KEY,
   title TEXT,
   year INTEGER
-)
+);
 
 CREATE TABLE character_books (
   id INTEGER PRIMARY KEY,
@@ -34,4 +34,4 @@ CREATE TABLE characters (
   motto TEXT,
   author_id INTEGER,
   series_id INTEGER
-)
+);
